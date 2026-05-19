fb76b1802985d89b66941914d9c4cb2da5b0a670
Kitsune7X <fox.dev7@proton.me>
2026-05-19 12:48:12 -0700

    .

---
+++ chapter03/Arrays/Program.cs
@@ -0,0 +1,76 @@
+﻿string[] names; // THis can reference any size array of strings
+// Allocate memory for four strings in an array
+names = new string[4];
+// Store items at these index positions.
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+// Loop through the names.
+for (int i = 0; i < names.Length; i++)
+{
+  // Output the item at index position i
+  WriteLine($"{names[i]} is at positon {i}.");
+}
+
+string[] names2 = ["Kate", "Jack", "Rebecca", "Tom"];
+
+for (int i = 0; i < names2.Length; i++)
+{
+  WriteLine($"{names2[i]} is at position {i}.");
+}
+
+// Two-dimensional array
+string[,] grid1 = // Two-dimensional array
+{
+  { "Alpha", "Beta", "Gamma", "Delta" },
+  { "Anne", "Ben", "Charlie", "Doug" },
+  { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine($"1st dimension, lower bound: {grid1.GetLowerBound(0)}");
+WriteLine($"1st dimension, upper bound: {grid1.GetUpperBound(0)}");
+WriteLine($"2nd dimension, lower bound: {grid1.GetLowerBound(1)}");
+WriteLine($"2nd dimension, upper bound: {grid1.GetUpperBound(1)}");
+
+// Loop through two-dimensional array
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+  for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+  {
+    WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+  }
+}
+
+// Jagged array
+// C# 11 and earlier must use curly braces and new[] expression
+// string[][] jagged = // An array of string arrays
+// {
+//   new []{ "Alpha", "Beta", "Gamma" },
+//   new []{ "Anne", "Ben", "Charlie", "Doug" },
+//   new []{ "Aardvark", "Bear" }
+// };
+
+// C# 12 and later can use collection expressions that use square brackets.
+string[][] jagged = // An array of string arrays.
+[
+  ["Alpha", "Beta", "Gamma"],
+  ["Anne", "Ben", "Charlie", "Doug"],
+  ["Aardvark", "Bear"],
+];
+
+WriteLine("Upper bound of the array of arrays is: {0}", jagged.GetUpperBound(0));
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+  WriteLine("Upper bound of array {0} is: {1}", arg0: array, arg1: jagged[array].GetUpperBound(0));
+}
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+  for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+  {
+    WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
+  }
+}
---
+++ chapter03/IterationStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿// int x = 0;
+// while (x < 10)
+// {
+//   WriteLine(x);
+//   x++;
+// }
+
+// Do while loop
+// const string? actualPassword = "Pa$$w0rd";
+// const int MAX_RETRIES = 3;
+// int retry = 0;
+//
+// string? password;
+// do
+// {
+//   Write("Enter your password: ");
+//   password = ReadLine();
+//   retry += 1;
+// } while (password != actualPassword && retry < MAX_RETRIES);
+//
+// if (retry >= MAX_RETRIES)
+// {
+//   WriteLine("Failed");
+//   return;
+// }
+// WriteLine("Correct!");
+
+// For loop
+for (int y = 0; y <= 10; y++)
+{
+  WriteLine(y);
+}
+
+for (int y = 0; y <= 10; y +=3)
+{
+  WriteLine(y);
+}
+
+// Foreach loop
+List<string> names = ["Adam", "Barry", "Charlie"];
+foreach (string name in names)
+{
+  WriteLine($"{name} has {name.Length} charaters");
+}
+
