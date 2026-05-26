7270ebf787806e2cd1bbbeb09e7c7cc78e664364
Kitsune7X <fox.dev7@proton.me>
2026-05-26 13:23:21 -0700

    type conversion

--- chapter03/CastingConverting/Program.cs
+++ chapter03/CastingConverting/Program.cs
@@ -1,11 +1,13 @@
-﻿int a = 10;
+﻿using static System.Convert; // To use the ToInt32 method
+
+int a = 10;
 double b = a; // An int can be safely cast into a double
 WriteLine($"a is {a}, b is {b}");
 
 
 // Casting double to int
 double c = 9.8;
-int d =  (int)c; // Compiler will throw an error if you do not explicitly cast
+int d = (int)c; // Compiler will throw an error if you do not explicitly cast
 WriteLine($"c is {c}, d is {d}"); // d loses the .8 part.
 
 // Cast large and small integers
@@ -15,3 +17,64 @@ WriteLine($"e is {e:N0}, f is {f:N0}");
 e = long.MaxValue;
 f = (int)e;
 WriteLine($"e is {e:N0}, f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+// Represent negative number in binary
+WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--)
+{
+  WriteLine("{0,12} {0,34:B32}", i);
+}
+
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+// Casting 32-bit integer to 16-bit integer
+long r = 0b_101000101010001100100111010100101010;
+int s = (int)r;
+WriteLine($"{r,38:B38} = {r}");
+WriteLine($"{s,38:B32} = {s}");
+
+// Using casting
+double g = 9.8;
+int h = ToInt32(g); // A method of System.Convert
+WriteLine($"g is {g}, h is {h}");
+
+// Rounding
+double[,] doubles =
+{
+  { 9.49, 9.5, 9.51 },
+  { 10.49, 10.5, 10.51 },
+  { 11.49, 11.5, 11.51 },
+  { 12.49, 12.5, 12.51 },
+  { -12.49, -12.5, -12.51 },
+  { -11.49, -11.5, -11.51 },
+  { -10.49, -10.5, -10.51 },
+  { -9.49, -9.5, -9.51 }
+};
+WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+  for (int y = 0; y < 3; y++)
+  {
+    Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7} ");
+  }
+
+  WriteLine("|");
+}
+
+WriteLine();
+
+// Control rounding
+foreach (double n in doubles)
+{
+  WriteLine(format:
+    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+    arg0: n,
+    arg1: Math.Round(value: n, digits: 0,
+      mode: MidpointRounding.AwayFromZero)
+  );
+}
